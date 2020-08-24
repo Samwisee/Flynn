e18cc0a0f443a3728a2f783c30a169353e29fcd0
Sam Cheney <samcheneyus@gmail.com>
2020-08-24 21:53:53 +1000

    all basic functionality working: cards, background success, randomization

---
+++ index.js
@@ -0,0 +1,11 @@
+animalsArray = ["african-grey-parrot", "alligator", "alpaca", "anteater", "antelope", "ape", "bat", "bee", "bowhead-whale", "buffalo", "butterfly", "camel", "cat", "chicken", "rooster", "cow", "crow", "dinosaur", "dog", "dove", "duck", "elephant", "falcon", "ferret", "frog", "giraffe", "guinea-pig", "hedgehog", "hippopotamus", "horse", "humpback-whale", "hyena", "komodo-dragon", "leopard", "lion", "lizard", "moose", "otter", "owl", "panda", "penguin", "pig", "rabbit", "raccoon", "rat", "rattlesnake", "rhinoceros", "robin", "scorpion", "shark", "sheep", "swan", "tiger", "turkey", "wolf", "yak", "zebra"]
+
+easyAnimalsArray = ["cow", "chicken", ]
+mediumAnimalsArray = []
+hardAnimalsArray = ["komodo-dragon", "butterfly", ]
+
+
+function shuffleAnimals (array){
+  const shuffled = array.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, 3);
+}
