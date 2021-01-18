68b8d4570d9bafe080ae7293627abf9ccf6e1fb8
Sam Cheney <samcheneyus@gmail.com>
2021-01-18 15:46:00 +1100

    separating ts name_game files

---
+++ name_game/name_game.js
@@ -0,0 +1,12 @@
+var familyArray = ["Sam", "Sim", "Luke", "Desiree", "Jenna", "Micheal", "Mack", "Wendy"];
+function allowDrop(event) {
+    event.preventDefault();
+}
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+}
